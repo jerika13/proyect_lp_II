6aa003564a8b3d4703f7d872fcd99033a81dd5b2
LAB-2 <LAB-2@LAB2-26>
2023-08-14 12:07:27 -0500

    creando repositorio

---
+++ src/main/java/upeu/edu/pe/proyecto_lp2/ProyectoLp2Application.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.proyecto_lp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLp2Application.class, args);
+	}
+
+}
